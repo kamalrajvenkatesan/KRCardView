74f9fbfe3cc076f75a2246876df841a25dc8e5c5
David Martínez <damarte86@gmail.com>
2018-12-27 12:22:29 +0100

    Added method for manual toogle the card view.
    Created method for check card view visibility.
    
    Added callback method after toogle the view.

--- KRCardView/Classes/CardViewController.swift
+++ KRCardView/Classes/CardViewController.swift
@@ -9,19 +9,16 @@
 import UIKit
 
 open class CardViewController: UIViewController {
-
- @IBOutlet weak var handleArea: UIView!
-
-  var visualEffectView = UIVisualEffectView()
-
-  var cardVisible: Bool = false
-  var runningAnimations = [UIViewPropertyAnimator]()
-  var animationProgressWhenInterrupted:CGFloat = 0
-  
-  override open func viewDidLoad() {
+    
+    @IBOutlet weak var handleArea: UIView!
+    
+    var visualEffectView = UIVisualEffectView()
+    
+    var cardVisible: Bool = false
+    var runningAnimations = [UIViewPropertyAnimator]()
+    var animationProgressWhenInterrupted:CGFloat = 0
+    
+    override open func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
-
 }
